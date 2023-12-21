854370d29f05b0cb0d0bfe2958b208b3f9e15f54
Chase Cole <chasewcole@gmail.com>
2023-12-21 14:55:53 -0800

    Setting up personal website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Your JavaScript code goes here
+// For example:
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to run after the DOM has loaded
+    console.log('DOM Loaded');
+});
+
